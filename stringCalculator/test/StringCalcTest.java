eb9f4f9bb6498daad3efc55ca0ec5a6f2df93be2
Vik <lem17902@spengergasse.at>
2017-09-13 17:54:52 +0200

    created: stringCalculator package src package test package stringCalc class stringCalcTest

--- stringCalculator/test/StringCalcTest.java
+++ stringCalculator/test/StringCalcTest.java
@@ -1,6 +1,11 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalcTest
 {
-
+    @Test
+    void emptyStringReturnNull(int act) {
+        assertEquals(0, act);
+    }
 }
