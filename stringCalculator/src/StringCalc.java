b414067d9191c8afc1962b1fb8ae6e3b4902958f
Vik <lem17902@spengergasse.at>
2017-09-25 16:16:58 +0200

    More tests, coding and refactoring.

--- stringCalculator/src/StringCalc.java
+++ stringCalculator/src/StringCalc.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class StringCalc {
 
     public int add(String numbers)
     {
-        return numbers.length();
+        int result=0;
+        if (numbers != null){
+            Scanner fi = new Scanner(numbers);
+            fi.useDelimiter(",");
+            while (fi.hasNextInt()){
+                result = result + fi.nextInt();
+            }
+            return result;
+        }
+        else
+            return 0;
     }
 }
--- stringCalculator/test/StringCalcTest.java
+++ stringCalculator/test/StringCalcTest.java
@@ -5,8 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringCalcTest
 {
     @Test
-    void emptyStringReturnNull() {
+    void emptyStringReturnsZero() {
+        init(0, "");
+    }
+
+    @Test
+    void oneStringReturnsOne() {
+        init(1, "1");
+    }
+
+    @Test
+    void threeCharacterStringReturnsSum() {
+        init(4, "2,2");
+    }
+
+    @Test
+    void nullStringReturnsZero() {
+        init(0, null);
+    }
+
+    private void init(int expected, String actual){
         StringCalc sc = new StringCalc();
-        assertEquals(0, sc.add(""));
+        assertEquals(expected, sc.add(actual));
     }
 }
