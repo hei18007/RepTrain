1b6f1fc4fff463338294537dc3dfd37eef625131
Vik <lem17902@spengergasse.at>
2017-09-25 18:15:39 +0200

    code style changed; codeStyleSettings.xml uploaded

--- stringCalculator/src/StringCalc.java
+++ stringCalculator/src/StringCalc.java
@@ -1,19 +1,20 @@
 import java.util.Scanner;
 
-public class StringCalc {
+public class StringCalc
+{
 
-    public int add(String numbers)
-    {
-        int result=0;
-        if (numbers != null){
-            Scanner fi = new Scanner(numbers);
-            fi.useDelimiter(",");
-            while (fi.hasNextInt()){
-                result = result + fi.nextInt();
-            }
-            return result;
-        }
-        else
-            return 0;
-    }
+	public int add(String numbers)
+	{
+		int result = 0;
+		if (numbers != null)
+		{
+			Scanner fi = new Scanner(numbers);
+			fi.useDelimiter(",");
+			while (fi.hasNextInt())
+			{
+				result = result + fi.nextInt();
+			}
+		}
+		return result;
+	}
 }
--- stringCalculator/test/StringCalcTest.java
+++ stringCalculator/test/StringCalcTest.java
@@ -4,28 +4,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalcTest
 {
-    @Test
-    void emptyStringReturnsZero() {
-        init(0, "");
-    }
+	@Test void emptyStringReturnsZero()
+	{
+		init(0, "");
+	}
 
-    @Test
-    void oneStringReturnsOne() {
-        init(1, "1");
-    }
+	@Test void oneStringReturnsOne()
+	{
+		init(1, "1");
+	}
 
-    @Test
-    void threeCharacterStringReturnsSum() {
-        init(4, "2,2");
-    }
+	@Test void threeCharacterStringReturnsSum()
+	{
+		init(4, "2,2");
+	}
 
-    @Test
-    void nullStringReturnsZero() {
-        init(0, null);
-    }
+	@Test void nullStringReturnsZero()
+	{
+		init(0, null);
+	}
 
-    private void init(int expected, String actual){
-        StringCalc sc = new StringCalc();
-        assertEquals(expected, sc.add(actual));
-    }
+	private void init(int expected, String actual)
+	{
+		StringCalc sc = new StringCalc();
+		assertEquals(expected, sc.add(actual));
+	}
 }
